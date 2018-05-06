1d5d2d9481a010aa9c9f8a166e9d502c9acbf6e5
Shane Rudolf <shane1@Shanes-MacBook-Pro.local>
2018-05-05 23:14:34 -0700

    add view port

--- storybook/storybook.js
+++ storybook/storybook.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry , View, Button} from 'react-native';
 import { getStorybookUI, configure } from '@storybook/react-native';
 
 // import stories
 configure(() => {
-  require('./stories');
+    require('./stories');
 }, module);
 
 // This assumes that storybook is running on the same host as your RN packager,
@@ -15,9 +15,34 @@ const StorybookUIRoot = getStorybookUI({ port: 7007, onDeviceUI: true });
 // https://github.com/storybooks/storybook/issues/2081
 // eslint-disable-next-line react/prefer-stateless-function
 class StorybookUIHMRRoot extends Component {
-  render() {
-    return <StorybookUIRoot />;
-  }
+    state = {
+        width: "100%",
+        height: "100%"
+    };
+
+    setIphone5 = () => {
+        this.setState({width: 320, height: 480});
+    };
+
+    setIphone6 = () => {
+        this.setState({width: 375, height: 667});
+    };
+
+    setIphoneX = () => {
+        this.setState({width: 2436, height: 1125});
+    };
+
+    render() {
+        const {width, height} = this.state;
+        return <View style={{width, height}}>
+            <StorybookUIRoot/>
+            <View style={{flexDirection: 'row'}}>
+                <Button onPress={this.setIphone5} title="iPhone 5" />
+                <Button onPress={this.setIphone6} title="iPhone 6" />
+                <Button onPress={this.setIphoneX} title="iPhone X" />
+            </View>
+        </View>;
+    }
 }
 
 AppRegistry.registerComponent('StorybookTutorial', () => StorybookUIHMRRoot);
