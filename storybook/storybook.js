7aa2823b366a6b48fa6a5dc197d9d45fa8dd5ddd
Shane Rudolf <shane1@Shanes-MacBook-Pro.local>
2018-05-05 23:33:18 -0700

    wrong imp

--- storybook/storybook.js
+++ storybook/storybook.js
@@ -17,29 +17,30 @@ const StorybookUIRoot = getStorybookUI({ port: 7007, onDeviceUI: true });
 class StorybookUIHMRRoot extends Component {
     state = {
         width: "100%",
-        height: "100%"
+        height: "100%",
+        aspectRatio: null
     };
 
     setIphone5 = () => {
-        this.setState({width: 320, height: 480});
+        this.setState({width: "32%", height: null, aspectRatio:320 / 700});
     };
 
     setIphone6 = () => {
-        this.setState({width: 375, height: 667});
+        this.setState({width: "37.5%", height: null, aspectRatio:375 / 867});
     };
 
-    setIphoneX = () => {
-        this.setState({width: 2436, height: 1125});
+    setIphone6P = () => {
+        this.setState({width: "41.4%", height: "73.6%"});
     };
 
     render() {
-        const {width, height} = this.state;
-        return <View style={{width, height}}>
+        const {width, height, aspectRatio} = this.state;
+        return <View style={{width, height, aspectRatio}}>
             <StorybookUIRoot/>
             <View style={{flexDirection: 'row'}}>
                 <Button onPress={this.setIphone5} title="iPhone 5" />
                 <Button onPress={this.setIphone6} title="iPhone 6" />
-                <Button onPress={this.setIphoneX} title="iPhone X" />
+                <Button onPress={this.setIphone6P} title="iPhone 6 Plus" />
             </View>
         </View>;
     }
